f06993838bba7cc19bbf38f076db163225414751
dave <David-Schmitz@gmx.net>
2014-12-25 21:45:00 +0100

    Issue #6:
    - fixed keyboard
    - fixed java
    - fixed tz
    - todo sudo nopasswd

--- vm/packer/debian-testing/scripts/java.sh
+++ vm/packer/debian-testing/scripts/java.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-sudo add-apt-repository ppa:webupd8team/java -y
-
+sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu vivid main" | tee -a /etc/apt/sources.list && \
+sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu vivid main" | tee -a /etc/apt/sources.list && \
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 && \
 sudo apt-get update
+
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 
 sudo apt-get install -y oracle-java8-installer
--- vm/packer/debian-testing/scripts/vagrant.sh
+++ vm/packer/debian-testing/scripts/vagrant.sh
@@ -2,8 +2,8 @@ echo "Add vagrant user ssh without a password (vagrant client includes its own p
 su vagrant -c "mkdir ~/.ssh && wget -qO- https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub >> ~/.ssh/authorized_keys"
 sudo chmod -R go-rwsx /home/vagrant/.ssh
 
-echo "Grant vagrant user sudo permissions without prompting for a password"
-sudo groupadd admin
-sudo usermod -G admin vagrant
-sudo cp /etc/sudoers /etc/sudoers.orig
-sudo sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+#echo "Grant vagrant user sudo permissions without prompting for a password"
+#sudo groupadd admin
+#sudo usermod -G admin vagrant
+#sudo cp /etc/sudoers /etc/sudoers.orig
+#sudo sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
