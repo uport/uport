0ebdc9aa06df3922e066d2666114e9f56a71869d
Schmitz <schmitda@AEVL006.fritz.box>
2016-01-24 20:29:13 +0100

    added additional docker packages

--- vm/packer/debian-testing/scripts/docker.sh
+++ vm/packer/debian-testing/scripts/docker.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo apt-get -y install docker.io
+sudo apt-get -y install docker.io docker-compose docker-registry
 sudo sed -i '/#DOCKER_OPTS=/c\DOCKER_OPTS="-s btrfs -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"' /etc/default/docker
 sudo sed -i '/GRUB_CMDLINE_LINUX=/c\GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"' /etc/default/grub
 sudo adduser vagrant docker
