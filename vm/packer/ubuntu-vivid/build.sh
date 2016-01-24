4940cf32845d11414417f3ebdc6b866071d5b1c6
David Schmitz <David.Schmitz@audi.de>
2016-01-24 11:18:48 +0100

    cleanup and reference to new release iso url for ubuntu

--- vm/packer/ubuntu-vivid/build.sh
+++ vm/packer/ubuntu-vivid/build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Zsync daily build image
-ISO_FILE="vivid-server-amd64.iso"
+ISO_FILE="ubuntu-15.04-server-amd64.iso"
 
 mkdir -p zsync
-(cd zsync && zsync -k $ISO_FILE.zsync -i $ISO_FILE http://cdimage.ubuntu.com/ubuntu-server/daily/current/$ISO_FILE.zsync)
+(cd zsync && zsync -k $ISO_FILE.zsync -i $ISO_FILE http://releases.ubuntu.com/15.04/ubuntu-15.04-server-amd64.iso.zsync)
 
 # Packer to create VM
 time packer build -force packer.json | tee build.log
